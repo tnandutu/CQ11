e740e783881f473a268f40b0ac480cc2998713d1
Teddy Nandutu <nandutut2013@gmail.com>
2024-08-25 16:33:46 +0300

    change

---
+++ Javascript-Assesment/assesment.js
@@ -0,0 +1,12 @@
+/*function printArray(a) {
+  var len = a.length, i = 0; if (len ==0)
+    console.log("Empty Array"); 
+} else {do {
+    console.log(a[i]); while(++i < len);
+  }
+}
+
+
+int a 1 = 1;
+if(a!=null)
+return 1; else return
